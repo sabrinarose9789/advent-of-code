ef5bad68ab31488966786eb31cd0df2b95b972cc
sabrinarose9789 <sssedovic@uchicago.edu>
2022-12-06 08:21:27 -0600

    completed day 4 and 5, added some helper scripts

---
+++ helper_scripts/stack.py
@@ -0,0 +1,14 @@
+class Stack:
+    def __init__(self):
+        self.stack = []
+
+    def push(self, value):
+        self.stack.append(value)
+
+    def pull(self):
+        value = self.stack[-1]
+        self.stack = self.stack[:-1]
+        return value
+    
+    def __str__(self):
+        return str(self.stack)
